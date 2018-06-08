061f8cf41293eeb981cd5d9723ef72e694cb96da
Nanachi <b01077016039@gmail.com>
2018-06-08 20:14:08 +0900

    판뒤집기 성공
    완성도 7%

---
+++ Assets/Scripts/ReverseBoard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReverseBoard : MonoBehaviour {
+    
+    void Start () {
+    }
+
+	void Update () {
+        Debug.Log(transform.eulerAngles.z);
+        transform.Rotate(new Vector3(0,0, (180-transform.eulerAngles.z)*0.05f));
+    }
+}
