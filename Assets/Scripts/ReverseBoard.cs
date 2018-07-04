78b19b4ee51a7e0cd38ca2e7b0b5b420df5b61e8
Nanachi <b01077016039@gmail.com>
2018-07-04 20:20:38 +0900

    레드, 블루 판 뒤집기 구현
    ...

--- Assets/Scripts/ReverseBoard.cs
+++ Assets/Scripts/ReverseBoard.cs
@@ -3,12 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ReverseBoard : MonoBehaviour {
-    
+
+    bool flag=false;
     void Start () {
     }
 
 	void Update () {
-        Debug.Log(transform.eulerAngles.z);
-        transform.Rotate(new Vector3(0,0, (180-transform.eulerAngles.z)*0.05f));
+        //Debug.Log(transform.eulerAngles.z);
+
+        if (flag)
+        {
+            transform.Rotate(new Vector3(0, 0, (180 - transform.eulerAngles.z) * 0.05f));
+        }
+        else
+        {
+            transform.Rotate(new Vector3(0, 0, (0 - transform.eulerAngles.z) * 0.05f));
+        }
+    }
+
+    public void SetFlag ()
+    {
+        flag = !flag;
+
     }
 }
