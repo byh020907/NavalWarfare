1df8a940559e3aafdbd58f052984ab56a1c186e8
Nanachi <b01077016039@gmail.com>
2018-07-13 18:51:29 +0900

    윗 판, 시간
    2018 7/13

---
+++ Assets/Scripts/InitScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitScript : MonoBehaviour {
+
+    public int N = 10;
+    public Transform tile;
+
+	// Use this for initialization
+	void Start () {
+        
+        Transform redBase = GameObject.Find("RedGround").transform;
+        Vector3 scale = redBase.transform.localScale;
+        //꼭짓점 위치 구함
+        Vector3 offsetPosition = new Vector3(-0.5f, 0.5f, -0.5f);
+
+        float O;
+        O = 1.0f/N;
+        offsetPosition.x += O/2;
+        offsetPosition.y -= O/2;
+        offsetPosition.z += O/2;
+
+        tile = Resources.Load<Transform>("Prefabs/Tile");
+        //Instantiate(tile, null).position = v2;
+
+        
+        for(int i=0; i<N; ++i)
+        {
+            for(int j=0; j<N; ++j)
+            {
+                Transform g=Instantiate(tile,redBase);
+                g.localPosition = offsetPosition;
+                g.localPosition += new Vector3(i*O, O/2, j *O);
+                g.transform.localScale = new Vector3(1.05f, 0.105f, 1.05f);
+                g.transform.localScale /= N;
+            }
+        }
+
+        Transform blueBase = GameObject.Find("BlueGround").transform;
+        scale = blueBase.transform.localScale;
+        //꼭짓점 위치 구함
+        offsetPosition = new Vector3(-0.5f, 0.5f, -0.5f);
+
+        O = 1.0f / N;
+        offsetPosition.x += O / 2;
+        offsetPosition.y -= O / 2;
+        offsetPosition.z += O / 2;
+
+        for (int i = 0; i < N; ++i)
+        {
+            for (int j = 0; j < N; ++j)
+            {
+                Transform g = Instantiate(tile, blueBase);
+                g.localPosition = offsetPosition;
+                g.localPosition += new Vector3(i * O, O/2, j * O);
+                g.transform.localScale = new Vector3(1.05f, 0.105f, 1.05f);
+                g.transform.localScale /= N;
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public int maxCount =20;
+    public Text text;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ss()
+    {
+        StartCoroutine("StartTimer");
+    }
+
+    public IEnumerator StartTimer()
+    {
+        for (int i = maxCount; i >= 0; --i)
+        {
+            text.text = i.ToString();
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
